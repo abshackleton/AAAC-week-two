02b05918b729612c74fac8caa1a0bad559664107
abshackleton <92082997+abshackleton@users.noreply.github.com>
2022-09-23 13:38:49 +0100

    Add files via upload

---
+++ week-two-challenge.py
@@ -0,0 +1,11 @@
+# create a folder call inputs
+# create a folder called reports
+# create a folder called results
+
+# on a regular basis scan the inputs folder and if there are files there check what type of file it is
+# xls, xlsx, and html files go to reports
+# csv, txt, json files go to results
+# any other files types can be deleted
+
+
+
